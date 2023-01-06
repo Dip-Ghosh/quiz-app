3580781e60244ffeef996687730a9fbf7320579f
Dip Ghosh <dip.ghosh@shohoz.com>
2023-01-06 21:36:29 +0600

    question load done

--- js/script.js
+++ js/script.js
@@ -1,4 +1,4 @@
-const quizData = [
+const quizQuestions = [
     {
         question: "What is the most used programming language in 2019?",
         a: "Java",
@@ -33,68 +33,30 @@ const quizData = [
     },
 ];
 
-const quiz = document.getElementById("quiz");
-const answerEls = document.querySelectorAll(".answer");
-const questionEl = document.getElementById("question");
-const a_text = document.getElementById("a_text");
-const b_text = document.getElementById("b_text");
-const c_text = document.getElementById("c_text");
-const d_text = document.getElementById("d_text");
-const submitBtn = document.getElementById("submit");
+const questions = document.getElementById('question');
+const optionA   = document.getElementById('option-a');
+const optionB   = document.getElementById('option-b');
+const optionC   = document.getElementById('option-c');
+const optionD   = document.getElementById('option-d');
+const submitBtn = document.getElementById('submit');
+let   count     = 0;
 
-let currentQuiz = 0;
-let score = 0;
-
-loadQuiz();
-
-function loadQuiz() {
-    deselectAnswers();
-
-    const currentQuizData = quizData[currentQuiz];
-
-    questionEl.innerText = currentQuizData.question;
-    a_text.innerText = currentQuizData.a;
-    b_text.innerText = currentQuizData.b;
-    c_text.innerText = currentQuizData.c;
-    d_text.innerText = currentQuizData.d;
+const loadQuestions = () => {
+    const currentQuestion = quizQuestions [count];
+    questions.innerHTML   = currentQuestion.question;
+    optionA.innerHTML     = currentQuestion.a;
+    optionB.innerHTML     = currentQuestion.b;
+    optionC.innerHTML     = currentQuestion.c;
+    optionD.innerHTML     = currentQuestion.d;
 }
 
-function getSelected() {
-    let answer = undefined;
+const loadQuiz = submitBtn.addEventListener('click', () => {
+    count++;
 
-    answerEls.forEach((answerEl) => {
-        if (answerEl.checked) {
-            answer = answerEl.id;
-        }
-    });
+    if (count < quizQuestions.length) loadQuestions();
+    else return undefined;
 
-    return answer;
-}
+})
 
-function deselectAnswers() {
-    answerEls.forEach((answerEl) => {
-        answerEl.checked = false;
-    });
-}
 
-submitBtn.addEventListener("click", () => {
-    // check to see the answer
-    const answer = getSelected();
-
-    if (answer) {
-        if (answer === quizData[currentQuiz].correct) {
-            score++;
-        }
-
-        currentQuiz++;
-        if (currentQuiz < quizData.length) {
-            loadQuiz();
-        } else {
-            quiz.innerHTML = `
-                <h2>You answered correctly at ${score}/${quizData.length} questions.</h2>
-                
-                <button onclick="location.reload()">Reload</button>
-            `;
-        }
-    }
-});
+document.addEventListener("DOMContentLoaded", loadQuestions);
