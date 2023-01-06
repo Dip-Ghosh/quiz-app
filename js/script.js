fb14e35f15b096efb167e6ace57b17dd76ed315d
Dip Ghosh <dip.ghosh@shohoz.com>
2023-01-06 23:45:33 +0600

    project done

--- js/script.js
+++ js/script.js
@@ -33,16 +33,20 @@ const quizQuestions = [
     },
 ];
 
+const quiz      = document.getElementById('quiz');
 const questions = document.getElementById('question');
+const answers   = document.querySelectorAll('.answer');
 const optionA   = document.getElementById('option-a');
 const optionB   = document.getElementById('option-b');
 const optionC   = document.getElementById('option-c');
 const optionD   = document.getElementById('option-d');
 const submitBtn = document.getElementById('submit');
 let   count     = 0;
+let   score     = 0;
+
 
 const loadQuestions = () => {
-    const currentQuestion = quizQuestions [count];
+    const currentQuestion = quizQuestions[count];
     questions.innerHTML   = currentQuestion.question;
     optionA.innerHTML     = currentQuestion.a;
     optionB.innerHTML     = currentQuestion.b;
@@ -50,11 +54,44 @@ const loadQuestions = () => {
     optionD.innerHTML     = currentQuestion.d;
 }
 
-const loadQuiz = submitBtn.addEventListener('click', () => {
-    count++;
+const selectedOption =  () => {
+    let selectedOption = null;
 
-    if (count < quizQuestions.length) loadQuestions();
-    else return undefined;
+    answers.forEach(answer => {
+        if (answer.checked ===true) selectedOption = answer.id;
+    })
+
+    return selectedOption;
+}
+
+
+const deSelectOption =  () => {
+    answers.forEach(answer => {
+        answer.checked = false;
+    })
+}
+
+
+submitBtn.addEventListener('click', () => {
+
+    if (selectedOption()) {
+
+        if (selectedOption() === quizQuestions[count].correct) {
+            score++;
+        }
+
+        count++;
+
+        if (count < quizQuestions.length) {
+            deSelectOption();
+            loadQuestions();
+        } else {
+            quiz.innerHTML = ` <h1 style="margin-top: 60px;text-align:center"> Congratulations!</h1>
+                <h2 class="justify-content" style="margin-bottom: 60px;text-align:center">You answered correctly at ${score}/${quizQuestions.length} questions.</h2>
+                               <button onclick="location.reload()">Reload</button>`;
+        }
+    }
+
 
 })
 
